27181708584eaee8a2f0015394ad41af518d7124
Elpistolero35 <lymfrdat@gmail.com>
2023-05-28 22:39:04 +0300

    most of the data inserted

---
+++ extraEquipmentdata.sql
@@ -0,0 +1,44 @@
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'GPS Navigation System', 'GPS', 10, 5);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'Child Car Seat', 'Child Seat', 5, 10);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'Roof Rack', 'Roof Rack', 15, 3);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'Ski Rack', 'Ski Rack', 12, 2);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'Bike Rack', 'Bike Rack', 8, 4);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'Snow Chains', 'Snow Chains', 6, 6);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'Car Cover', 'Car Cover', 8, 7);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'Portable Wi-Fi Hotspot', 'Wi-Fi Hotspot', 10, 5);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'Roof Cargo Box', 'Cargo Box', 20, 2);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'Portable DVD Player', 'DVD Player', 5, 3);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'Car Phone Mount', 'Phone Mount', 3, 8);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'First Aid Kit', 'First Aid Kit', 4, 10);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'Car Vacuum Cleaner', 'Vacuum Cleaner', 7, 6);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'Car Air Freshener', 'Air Freshener', 2, 15);
+
+INSERT INTO extra_equipment (equipment_id, description, equipment_name, price, quantity)
+VALUES (equipment_seq.nextval, 'Portable Air Compressor', 'Air Compressor', 8, 4);
---
+++ staffdata.sql
@@ -0,0 +1,74 @@
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Ahmed', 'Address 1', 'Ali', '018635906', 'Manama', '39654321', 1, 1);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Mohammed', 'Address 2', 'Khalid', '191939729', 'Riffa', '39123456', 2, 1);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Abdullah', 'Address 3', 'Saeed', '075003766', 'Muharraq', '39234567', 2, 1);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Fatima', 'Address 4', 'Hassan', '341171842', 'Hamad Town', '39345678', 3, 1);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Ali', 'Address 5', 'Mohammed', '370309445', 'A''ali', '39456789', 5, 1);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Hassan', 'Address 6', 'Ahmed', '968634549', 'Isa Town', '39567890', 1, 2);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Omar', 'Address 7', 'Khalifa', '599895419', 'Sitra', '39678901', 2, 2);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Sara', 'Address 8', 'Abdulrahman', '890568600', 'Budaiya', '39789012', 2, 2);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Nora', 'Address 9', 'Hamed', '673481775', 'Jidhafs', '39890123', 5, 2);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Yousef', 'Address 10', 'Khaled', '916899167', 'Al-Malikiyah', '39901234', 4, 2);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Jassim', 'Address 11', 'Saleh', '059124166', 'Jid Ali', '39123456', 1, 3);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Huda', 'Address 12', 'Khalifa', '994665707', 'Sanabis', '39234567', 2, 3);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Ahmed', 'Address 13', 'Mohammed', '872317579', 'Tubli', '39345678', 4, 3);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Rana', 'Address 14', 'Ali', '773643662', 'Durrat Al Bahrain', '39456789', 5, 3);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Majed', 'Address 15', 'Hassan', '655159297', 'Gudaibiya', '39567890', 6, 3);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Layla', 'Address 16', 'Saeed', '884304216', 'Salmabad', '39678901', 1, 4);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Khalid', 'Address 17', 'Abdulrahman', '789876543', 'Jurdab', '39789012', 2, 4);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Hanan', 'Address 18', 'Mohammed', '898765432', 'Diyar Al Muharraq', '39890123', 2, 4);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Faisal', 'Address 19', 'Ahmed', '987654321', 'Amwaj Islands', '39901234', 4, 4);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Qaima ', 'Address 20', 'Dallal', '876543210', 'Al Hidd', '39123456', 1, 5);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Azahar', 'Address 20', 'Amini', '754398308', 'Gudaibiya', '39123456', 2, 5);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Zahir ', 'Address 20', 'Mourad', '871546754', 'Sanabis', '39123456', 2, 5);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Syed ', 'Address 20', 'Arif', '924810701', 'Al Hidd', '39123456', 5, 5);
+
+INSERT INTO Staff (staff_id, first_name, address, last_name, cpr_number, city, phone_number, Job_id, store_id) VALUES
+(staff_seq.nextval, 'Aiza ', 'Address 20', 'Karam', '963305637', 'Tubli', '39123456', 6, 5);
+
+
