5e502d5baec17495e38f2a66d88d39f11c86bbd3
CodeMolestor <alawyrose@gmail.com>
2023-06-06 05:26:06 +0300

    car rental data

--- CarRentalData.sql
+++ CarRentalData.sql
@@ -1,5 +1,62 @@
-INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, start_date, staff_id)
-VALUES (rental_seq.nextval, 3001, 1023, 7, TO_DATE('2023-06-01', 'YYYY-MM-DD'), 7001);
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3001, 1023, 7, 50, TO_DATE('2023-06-01', 'YYYY-MM-DD'), 7001);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3002, 1024, 5, 50, TO_DATE('2022-07-01', 'YYYY-MM-DD'), 7002);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3003, 1025, 12, 50, TO_DATE('2022-6-23', 'YYYY-MM-DD'), 7003);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3004, 1026, 3, 50, TO_DATE('2022-07-01', 'YYYY-MM-DD'), 7004);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3005, 1027, 7, 50, TO_DATE('2021-12-06', 'YYYY-MM-DD'), 7005);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3006, 1028, 15, 50, TO_DATE('2023-01-7', 'YYYY-MM-DD'), 7006);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3007, 1028, 14, 50, TO_DATE('2022-11-27', 'YYYY-MM-DD'), 7007);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3008, 1029, 2, 50, TO_DATE('2021-04-20', 'YYYY-MM-DD'), 7008);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3009, 1030, 8, 50, TO_DATE('2022-04-03', 'YYYY-MM-DD'), 7009);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3010, 1031, 5, 50, TO_DATE('2023-02-17', 'YYYY-MM-DD'), 7010);
+--11
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3011, 1032, 16, 50, TO_DATE('2023-02-23', 'YYYY-MM-DD'), 7011);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3012, 1033, 15, 50, TO_DATE('2022-07-30', 'YYYY-MM-DD'), 7012);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3013, 1034, 2, 50, TO_DATE('2021-10-01', 'YYYY-MM-DD'), 7013);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3014, 1035, 5, 50, TO_DATE('2023-08-09', 'YYYY-MM-DD'), 7014);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3015, 1036, 20, 50, TO_DATE('2021-01-29', 'YYYY-MM-DD'), 7015);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3016, 1037, 11, 50, TO_DATE('2022-02-09', 'YYYY-MM-DD'), 7016);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3017, 1038, 23, 50, TO_DATE('2023-04-26', 'YYYY-MM-DD'), 7017);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3018, 1039, 5, 50, TO_DATE('2023-05-04', 'YYYY-MM-DD'), 7018);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3019, 1040, 7, 50, TO_DATE('202-09-026', 'YYYY-MM-DD'), 7019);
+
+INSERT INTO Car_rental (rental_id, customer_id, car_id, rent_duration, COST_RENT, start_date, staff_id)
+VALUES (rental_seq.nextval, 3020, 1041, 8, 50, TO_DATE('2022-12-06', 'YYYY-MM-DD'), 7020);
 
 
-set SERVEROUTPUT ON;
+
